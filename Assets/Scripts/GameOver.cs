832386b647c6c084675f8ad43d213bf664fdb8dd
TheZombieCloud <alex.magamer@gmail.com>
2020-06-10 01:02:55 -0400

    new changes

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    private Camera camera;
+    private float cameraWidth;
+    private float cameraPos;
+
+    public GameObject ball;
+    private float ballWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+        cameraWidth = camera.orthographicSize * camera.aspect;
+        cameraPos = camera.transform.position.x;
+        ballWidth = (ball.transform.GetChild(0).transform.position.y - ball.transform.GetChild(1).transform.position.y)/2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ball.transform.position.x <= cameraPos - cameraWidth - ballWidth || ball.transform.position.x >= cameraPos + cameraWidth + ballWidth) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image image;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (image.enabled && (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))) {
+            image.enabled = false;
+        }
+    }
+}
